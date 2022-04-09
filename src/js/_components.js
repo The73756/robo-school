89ff62d421d8b5548477d52d0e0b717da6494812
The73756 <the73756the@gmail.com>
2022-04-09 16:45:00 +0900

    initial commit

---
+++ src/js/_components.js
@@ -0,0 +1,7 @@
+import './components/smooth-scroll';
+import './components/tooltip';
+import './components/modals';
+import './components/slider';
+import './components/validate';
+import './components/tabs';
+import './components/custom-scroll';
---
+++ src/js/components/tooltip.js
@@ -0,0 +1,16 @@
+import { createPopper, right} from '@popperjs/core';
+
+const el = document.querySelector('.tooltip__btn');
+const tooltip = document.querySelector('.tooltip__txt');
+
+createPopper(el, tooltip, {
+  placement: 'top-start',
+  modifiers: [
+    {
+      name: 'offset',
+      options: {
+        offset: [0, 10],
+      },
+    },
+  ],
+});
